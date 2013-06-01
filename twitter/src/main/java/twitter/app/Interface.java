8d1d4c492ebfe2672752194ab6ef24a9e98887e9
Antoxa653 <anton.rykov@gmail.com>
2013-06-01 22:30:00 +0300

    FixedTimeLine

--- twitter/src/main/java/twitter/app/Interface.java
+++ twitter/src/main/java/twitter/app/Interface.java
@@ -23,55 +23,49 @@ public class Interface extends JFrame {
 	JFrame frame;
 	public Interface() {
 		frame = new JFrame("Twitter Application");
-		frame.setSize(1000, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1000, 400);				
 	}
 	
-	public void mainInterface() throws TwitterException{		
-		//JFrame frame = new JFrame("Twitter Application");
-		//frame.setSize(1000, 400);
-		Box box1 = Box.createHorizontalBox();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public void mainInterface() throws TwitterException{	
 		sendButton = new JButton("Send");
-		updateButton = new JButton("Update");
+		updateButton = new JButton("Update");	
+		Box box1 = Box.createHorizontalBox();
 		box1.add(sendButton);
-		box1.add(Box.createHorizontalStrut(150));
-		box1.add(updateButton);
-		Box box2 = Box.createHorizontalBox();
+		box1.add(Box.createHorizontalStrut(100));
+		box1.add(updateButton);		
 		statusArea = new JTextArea(10, 20);
 		statusArea.setLineWrap(true);
 		statusArea.setWrapStyleWord(true);		
 		list = listCreation();
-		list.setVisibleRowCount(20);
-		list.setSize(200, 200); 		
+		Box box2 = Box.createHorizontalBox();				
 		box2.add(statusArea);
 		box2.add(Box.createHorizontalStrut(10));
-		box2.add(displayList());		
+		box2.add(list);		
 		Box mainBox =Box.createVerticalBox();
 		mainBox.setBorder(new EmptyBorder(12,12,12,12));
 		mainBox.add(box1);
 		mainBox.add(Box.createVerticalStrut(10));
 		mainBox.add(box2);
 		frame.setContentPane(mainBox);		
-		pack();
+		frame.pack();
 		frame.setResizable(false);		
 		sendButton.addActionListener(new ButtonEventListener());
 		updateButton.addActionListener(new UpdateButtonListener());
 		frame.setVisible(true);
+		
 	}
 	public JList<String> listCreation() throws TwitterException{
 		UserTimeLine utl =new UserTimeLine();	
 		list =new JList<String>(utl.getTimeLine());
 		return list;
 	}
-	public JList<String> displayList() throws TwitterException{
-		list = listCreation();
-		list.setVisibleRowCount(20);
-		list.setSize(200, 200);
-		return list;
-		
-	}
-	
-	
+	public void updateList() throws TwitterException{				
+		//list = listCreation();
+		//list.setVisibleRowCount(20);
+		//list.setSize(200, 200);
+		mainInterface();						
+	}	
 	
 	class ButtonEventListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
@@ -94,8 +88,8 @@ public class Interface extends JFrame {
 	class UpdateButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent e) {				
 					try {
-						list = listCreation();
-						mainInterface();
+						//list = listCreation();
+						updateList();
 					} catch (TwitterException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
